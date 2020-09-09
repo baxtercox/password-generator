0c42b3f67399f68e2aa54afd3edb098911d047b8
Baxter Cox <baxtercoxrtr@gmail.com>
2020-09-08 21:41:07 -0400

    final push

--- script.js
+++ script.js
@@ -7,10 +7,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-  console.log("Hello World");
 }
-
+//genertate password function
 function generatePassword() {
   console.log("Generate password HERE");
   var length = Number(
@@ -22,57 +20,70 @@ function generatePassword() {
   console.log("User chose X Length: ", length);
 
   if (length < 8) {
-    alert("Try again, please chose a valid length");
+    alert(
+      "Try again, the password length you have selected was under 8 characters"
+    );
     return;
   }
 
   if (length > 128) {
-    alert("Try again, please chose a valid length");
+    alert(
+      "Try again, the password length you have selected was over 128 characters"
+    );
     return;
   }
 
   console.log("you passed step 1");
-
+//these are the options to pick from
   var numerical = "0123456789";
   var LowerCase = "abcdefghijklmnopqrstuvwxyz";
   var upperCase = LowerCase.upperCase;
   var special = "~!@#$%^&*()_+=";
   var chosenCharacters = "";
-
+//these are confirm statements
   var useNumerical = confirm("Would you like to us Numerical characters?");
   var useLowerCase = confirm("Would you like to use Lowercase characters?");
   var useUpperCase = confirm("Would you like to Uppercase characters?");
   var useSpecial = confirm("would you like to use special characters?");
 
   console.log({ useNumerical, useLowerCase, useUpperCase, useSpecial });
-
+//the "if statements"
   if (useNumerical) {
-    chosenCharacters+= numerical;
+    chosenCharacters += numerical;
   }
   if (useLowerCase) {
-    chosenCharacters+= LowerCase;
+    chosenCharacters = chosenCharacters.concat(LowerCase);
   }
   if (useUpperCase) {
-    chosenCharacters+= upperCase;
+    chosenCharacters += upperCase;
   }
   if (useSpecial) {
-    chosenCharacters+= special;
+    chosenCharacters += special;
   }
 
-//create for loop
+  console.log("chosenCharacters", chosenCharacters);
 
-
-  console.warn(chosenCharacters);
-
-
-
-
-
+  return randomize(chosenCharacters, length);
 }
+//the actual generator 
+function randomize(options, length) {
+  options = options.split("");
+  var password = [];
 
-function randomize(options) {
-  options = options.split('');
-  var randIndex = Math.floor(Math.random() * options.length);
+  console.log(options);
+  console.log("length", length);
+  console.log("randIndex", randIndex);
+  //for loop that uses password length
+
+  for (var i = 0; i < length; i++) {
+    var randIndex = Math.floor(Math.random() * options.length);
+    password.push(options[randIndex]);
+    console.log("password", password);
+  }
+
+  password = password.join("");
+
+  return password;
 }
 
 //add event listener to genrerate button
